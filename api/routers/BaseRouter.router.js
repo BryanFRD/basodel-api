05f9a718dbbde86d01f07657546c47194dcaf218
Bryan <bryanferrando59@gmail.com>
2022-09-06 17:53:18 +0200

    Changes

--- api/routers/BaseRouter.router.js
+++ api/routers/BaseRouter.router.js
@@ -18,35 +18,41 @@ class BaseRouter {
     // CREATE
     this.router.post('/', async (req, res) => {
       const response = await this.controller.create({body: req.body});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
     
     // READ
     this.router.get('/', async (req, res) => {
       const response = await this.controller.get({body: req.body});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
     
     this.router.get('/:id', async (req, res) => {
       const response = await this.controller.get({id: req.params.id, body: req.body});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
     
     //UPDATE
     this.router.put('/', async (req, res) => {
       const response = await this.controller.update({body: req.body});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
     
     this.router.put('/:id', async (req, res) => {
       const response = await this.controller.update({id: req.params.id, body: req.body});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
     
     // DELETE
     this.router.delete('/:id', async (req, res) => {
       const response = await this.controller.delete({id: req.params.id});
-      res.send(response);
+      
+      res.status(response?.statusCode ?? res.statusCode).send(response.content ?? response);
     });
   }
   
