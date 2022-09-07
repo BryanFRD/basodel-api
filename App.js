f780f0e15041de6d6253b2eff1e47591d2ca3f1e
Bryan <bryanferrando59@gmail.com>
2022-09-07 20:52:06 +0200

    Managed error

--- App.js
+++ App.js
@@ -4,16 +4,26 @@ const morgan = require('morgan');
 const routers = require('./api/routers');
 const DB = require('./api/db/db');
 
-const app = express();
 
-app.use(morgan('dev')).use(express.json());
+const start = async () => {
+  const err = await DB.sync()
+  .then(() => console.log('Database synchronized!'))
+  .catch(err => err);
+  
+  if(err){
+    console.log(`Error while trying to synchronize with the database! \nError: ${err}`);
+    return;
+  }
+  
+  const app = express();
 
-DB.sync()
-.then(() => console.log('Database synchronized!'))
-.catch(err => console.log(`Error while trying to synchronize with the database \nError:${err}`));
-
-for(const route in routers){
-  app.use(`/${route}`, new routers[route]().router);
+  app.use(morgan('dev')).use(express.json());
+  
+  for(const route in routers){
+    app.use(`/${route}`, new routers[route]().router);
+  }
+  
+  app.listen(process.env.SERVER_PORT, () => console.log(`Basodel-API started on port ${process.env.SERVER_PORT}.`));
 }
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Basodel-API started on port ${process.env.SERVER_PORT}.`));
+start();
