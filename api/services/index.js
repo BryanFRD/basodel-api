0d1a5e5aa92ad6759dd300968818b2808dc6730e
BryanFRD <bryanferrando59@gmail.com>
2022-09-08 09:53:35 +0200

    Changes

--- api/services/index.js
+++ api/services/index.js
@@ -1,3 +1,10 @@
 exports.UserCredential = require('./UserCredential.service');
 exports.UserAccount = require('./UserAccount.service');
 exports.User = require('./User.service');
+exports.ChatMessage = require('./ChatMessage.service');
+exports.GameHistory = require('./GameHistory.service');
+exports.GameHistory_UserAccount = require('./GameHistory_UserAccount.service');
+exports.Purchase = require('./Purchase.service');
+exports.Report = require('./Report.service');
+exports.ReportStatus = require('./ReportStatus.service');
+exports.Role = require('./Role.service');
