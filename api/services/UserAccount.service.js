82a15bccbe383087b4dd7469be3c96e9a2292611
BryanFRD <bryanferrando59@gmail.com>
2022-09-09 11:48:47 +0200

    Changes

--- api/services/UserAccount.service.js
+++ api/services/UserAccount.service.js
@@ -2,10 +2,6 @@ const BaseService = require('./BaseService.service');
 
 class UserAccount extends BaseService {
   
-  insert = async (model, validate, params) => {
-    return {statusCode: 405, content: "You must use 'user' request in order to create an account with its credential!"};
-  }
-  
 }
 
 module.exports = UserAccount;
